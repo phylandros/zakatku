ce16f71f51f0bf4e58de321e542e602c0c97d5b4
phylandros <48447281+phylandros@users.noreply.github.com>
2023-10-31 00:07:06 +0700

    update pembayaran

---
+++ app/src/main/java/com/project/zakatku/PembayaranActivity.java
@@ -0,0 +1,14 @@
+package com.project.zakatku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PembayaranActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pembayaran);
+    }
+}
